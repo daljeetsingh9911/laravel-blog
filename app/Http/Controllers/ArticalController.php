b34d0e6864610c659b105b4ed4954e7980d0faf7
Daljeet singh <daljeetsingh9911@gmail.com>
2023-12-05 10:32:38 +0100

    setup porject and add breeze package for authentication added migrations

---
+++ app/Http/Controllers/ArticalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Artical;
+use Illuminate\Http\Request;
+
+class ArticalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Artical $artical)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Artical $artical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Artical $artical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Artical $artical)
+    {
+        //
+    }
+}
